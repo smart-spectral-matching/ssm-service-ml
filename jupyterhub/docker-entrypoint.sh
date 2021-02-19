2e661bd380fa7432bac92db790f6199711761cd7
Gibbs, Ian <gibbsib@ornl.gov>
2021-02-18 19:25:16 -0500

    feature: disable jupyterhub ssl

--- jupyterhub/docker-entrypoint.sh
+++ jupyterhub/docker-entrypoint.sh
@@ -8,8 +8,6 @@ echo "${JPYTR_USER}:${JPYTR_PASS}" | chpasswd
 chown -R ${JPYTR_USER}:${JPYTR_PASS} /srv/jupyterhub
 jupyterhub --debug \
   --config    /etc/jupyterhub/jupyterhub_config.py \
-  --ssl-cert  /etc/jupyterhub/server.cert \
-  --ssl-key   /etc/jupyterhub/server.key \
   --port ${JPYTR_LISTEN_PORT} \
   --ip 0.0.0.0
 
