6c8cb42ab87f4d9e37b13fe2eb87f16135dfce5b
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-02-22 09:59:18 -0500

    Fix small indent in config

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,6 +1,6 @@
 # Configuration file for jupyterhub.
 
- c.JupyterHub.base_url = '/machine-learning/training/'
+c.JupyterHub.base_url = '/machine-learning/training/'
 
 c.JupyterHub.spawner_class = 'simple'
 c.Spawner.notebook_dir = '/srv/jupyterhub/notebooks'
