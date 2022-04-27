e284e1ebd6a11226392ad506e660ad5cf8c818ab
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2022-04-27 15:03:05 +0000

    Re #19 - modify group setup for JupyterHub config + pin traitlets==5.1.1 in Dockerfile

--- jupyterhub/docker-entrypoint.sh
+++ jupyterhub/docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# NOTE: This must match jupyterhub_config.py under c.LocalAuthenticator.group_whitelist
+JPYTR_GROUP="ssm"
+
 which jupyterhub || exit 1
 
+# Setup user and password
 useradd -M ${JPYTR_USER}
 echo "${JPYTR_USER}:${JPYTR_PASS}" | chpasswd
+chown -R ${JPYTR_USER} /srv/jupyterhub
 
-chown -R ${JPYTR_USER}:${JPYTR_PASS} /srv/jupyterhub
+# Setup group
+groupadd ${JPYTR_GROUP}
+adduser ${JPYTR_USER} ${JPYTR_GROUP}
+
 jupyterhub --debug \
   --config    /etc/jupyterhub/jupyterhub_config.py \
   --port ${JPYTR_LISTEN_PORT} \
