bff654a74cd1e798dcd3b5514a9f13431c3a99ad
r8s <r8s@localhost>
2025-03-05 12:29:51 -0500

    Added tests for spectra matching functions
    Added tests for the new spectra matching functions.

--- tests/test_ssm_ml.py
+++ tests/test_ssm_ml.py
@@ -94,6 +94,24 @@ def test_database():
     )
     assert filter_json == db_filter
 
+def test_interpolate_spectra():
+    '''
+    Test spectra interpolation
+    '''
+    
+    spectra1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 0, 1, 2, 1, 0, 1, 2, 3]]
+    spectra2 = [[1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5], [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    
+    spectra3 = ssmml.interpolate_spectra(spectra1, spectra2)
+    
+    for i in range(len(spectra3[0])):
+        
+        # Spectra 2 should have been cast into the exact x values as spectra 1
+        assert spectra3[0][i] == spectra1[0][i]
+        
+        # The first two x values are before the start of spectra2, so replicate its first value
+        # The rest of the values will be halfway between the integer y values because they are half way between the x values
+        assert spectra3[1][i] == [1, 1, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5]
  
 def test_normalize_features():
     '''
@@ -103,12 +121,92 @@ def test_normalize_features():
     
     features = ssmml.normalize_features(features)
     
-    #Check that the minima are 0s, the maximas are 1s, and the middle values are properly porportnate
+    #Check that the minima are 0s, the maximas are 1s, and the middle values are properly porportonate
     assert [0, 0, 1] == features[0]
     assert [0.5, 0.2, 0.42857142857142855] == features[1]
     assert [1, 1, 0] == features[2]
+    
+def test_pearson_correlation_coefficient():
+    '''
+    Test Pearson correlation coefficient calculation.
+    '''
+    
+    # Three spectra, with the first two orthogonal
+    spectra1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]]
+    spectra2 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 1, 2, 1, 2, 1, 2, 1, 2, 1]]
+    spectra3 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 2]]
+    
+    # Normalize the spectra
+    features = ssmml.normalize_features([spectra1[1], spectra2[1], spectra3[1]])
+    spectra1[1] = features[0]
+    spectra2[1] = features[1]
+    spectra3[1] = features[2]
+    
+    # The maximum PCC is 1 when two spectra are identical
+    assert 1.0 == ssmml.pearson_correlation_coefficient(spectra1, spectra1)
+    
+    # Two maximally dissimilar spectra should have PCC -1
+    assert -1.0 == ssmml.pearson_correlation_coefficient(spectra1, spectra2)
+    
+    # A more random spectra should have a value between the two extremes
+    pcc3 = ssmml.pearson_correlation_coefficient(spectra1, spectra3)
+    assert -1 < pcc3 and 1 > pcc3
+    
+def test_squared_euclidean_cosine():
+    '''
+    Test the squared Euclidean cosine
+    '''
+    
+    # Three spectras with the first two orthogonal
+    spectra1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]]
+    spectra2 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 1, 2, 1, 2, 1, 2, 1, 2, 1]]
+    spectra3 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 2]]
+    
+    # Normalize the spectra
+    features = ssmml.normalize_features([spectra1[1], spectra2[1], spectra3[1]])
+    spectra1[1] = features[0]
+    spectra2[1] = features[1]
+    spectra3[1] = features[2]
+    
+    # The maximum distance is equal to the square root of the number of dimensions
+    max_v = math.sqrt(len(spectra1[0]))
+    
+    # Identical vectors have no difference in the angle between them
+    assert 0.0 == ssmml.squared_euclidean_cosine(spectra1, spectra1)
+    
+    # The maximum angular difference occurs between orthogonal vectors
+    assert max_v == ssmml.squared_euclidean_cosine(spectra1, spectra2)
+    
+    # Any other spectra should have a value between those two extremes
+    sec3 = ssmml.squared_euclidean_cosine(spectra1, spectra3)
+    assert 0 < sec3 and max_v > sec3
+    
+def test_squared_first_difference_euclidean_cosine():
+    '''
+    Test the squared first difference Euclidean cosine
+    '''
+    
+    # Three spectras with the first two orthogonal
+    spectra1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]]
+    spectra2 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 1, 2, 1, 2, 1, 2, 1, 2, 1]]
+    spectra3 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 2]]
+    
+    # Normalize the spectra
+    features = ssmml.normalize_features([spectra1[1], spectra2[1], spectra3[1]])
+    spectra1[1] = features[0]
+    spectra2[1] = features[1]
+    spectra3[1] = features[2]
+    
+    # Identical spectra have maximum similarity (1)
+    assert 1.0 == ssmml.squared_euclidean_cosine(spectra1, spectra1)
+    
+    # Orthogonal spectra have minimum similarity (0)
+    assert 0.0 == ssmml.squared_euclidean_cosine(spectra1, spectra2)
+    
+    # Any other spectra should have a value between those two extremes
+    sec3 = ssmml.squared_euclidean_cosine(spectra1, spectra3)
+    assert 0 < sec3 and 1 > sec3
 
- 
 def test_train():
     '''
     Test the training of a model
@@ -120,3 +218,32 @@ def test_train():
     
     #Check that it works
     classifier.decision_function([[0.1,0.1]])
+    
+def test_unit_normalized_euclidean_distance():
+    '''
+    Test Pearson correlation coefficient calculation.
+    '''
+    
+    # Three spectras with the first two orthogonal
+    spectra1 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]]
+    spectra2 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 1, 2, 1, 2, 1, 2, 1, 2, 1]]
+    spectra3 = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 2]]
+    
+    # Normalize the spectra
+    features = ssmml.normalize_features([spectra1[1], spectra2[1], spectra3[1]])
+    spectra1[1] = features[0]
+    spectra2[1] = features[1]
+    spectra3[1] = features[2]
+    
+    # The maximum distance is equal to the square root of the number of dimensions
+    max_v = math.sqrt(len(spectra1[0]))
+    
+    # Identical vectors have no difference in the angle between them
+    assert 0.0 == ssmml.unit_normalized_euclidean_distance(spectra1, spectra1)
+    
+    # The maximum angular difference occurs between orthogonal vectors
+    assert max_v == ssmml.unit_normalized_euclidean_distance(spectra1, spectra2)
+    
+    # Any other spectra should have a value between those two extremes
+    uned3 = ssmml.unit_normalized_euclidean_distance(spectra1, spectra3)
+    assert 0 < uned3 and max_v > uned3
